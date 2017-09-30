b18b53a8307288106d8ced7cd8d616dfa744007f
dtrunov <dtrunov@luxoft.com>
2017-09-30 23:01:42 +0300

    Test thread

--- thread/main.cpp
+++ thread/main.cpp
@@ -14,16 +14,14 @@ void *thread_main() {
 
 int main() {
   std::fstream f("/home/box/main.pid", std::ios_base::in | std::ios_base::out | std::ios_base::app);
-  std::shared_ptr<std::fstream> fptr(&f, [](std::fstream *fptr) {
-    std::cout << "close main.pid";
-    fptr->close();
-  });
 
-  if (!(fptr->is_open())) {
+
+  if (!(f.is_open())) {
     std::cout << "Failed during open file\n";
     return 1;
   }
-  (*fptr) << getpid();
+  f<< getpid();
+  f.close();
 
   std::thread tr1(thread_main);
   tr1.join();
